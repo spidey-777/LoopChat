84d7b5bd699b81cb31f04a7b094dcb6169ec6cda
spidey-777 <spidey4952@gmail.com>
2025-07-26 15:49:15 +0530

    first commit

---
+++ server/user/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+
+dotenv.config(); //
+connectDb();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(` Server is running on port ${PORT}`);
+});
