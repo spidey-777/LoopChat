3bd72528ddd71aa13e17f33f25b3248a3de402fd
spidey-777 <spidey4952@gmail.com>
2025-08-09 12:01:24 +0530

    implimented socket in chat service

--- server/chat/src/index.ts
+++ server/chat/src/index.ts
@@ -3,17 +3,18 @@ import dotenv from "dotenv"
 import connectDb from './config/db.js';
 import ChatRoutes from "./routes/chat.js"
 import cors from 'cors';
+import { app,server } from './config/socket.js';
 
 dotenv.config();
 
 connectDb();
 
-const app = express()
+
 app.use(express.json())
 app.use(cors())
 app.use('/api/v1',ChatRoutes);
 
 const PORT = process.env.PORT
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`);
 })
